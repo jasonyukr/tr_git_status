dd76cff5f4dbbd931ba5185b400c385554c06ae0
Jason Yu <jasonyu.kr@gmail.com>
2025-02-20 16:01:17 +0900

    Add quote if with whitespace

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@ use std::env;
 use lazy_static::lazy_static;
 use regex::Regex;
 
-// Declare a global variable to hold the Regex
 lazy_static! {
     /*
-     * regular expression for
+     * regular expression for matching
      *   A -> B
      *   A -> "B"
      *   "A" -> B
@@ -36,11 +35,20 @@ fn parse_rename(line: &str) -> Option<(&str, &str)> {
 #[test]
 fn test_parse_rename() {
     assert_eq!(parse_rename(r#"AAA.txt"#), None);
+    assert_eq!(parse_rename(r#""AAA space.txt""#), None);
     assert_eq!(parse_rename(r#"AAA.txt->CCC.txt"#), None);
     assert_eq!(parse_rename(r#""AAA space.txt" -> "CCC space.txt""#), Some(("AAA space.txt", "CCC space.txt")));
     assert_eq!(parse_rename(r#""AAA space.txt" -> CCC.txt"#), Some(("AAA space.txt", "CCC.txt")));
     assert_eq!(parse_rename(r#"AAA.txt -> "CCC space.txt""#), Some(("AAA.txt", "CCC space.txt")));
     assert_eq!(parse_rename(r#"AAA.txt -> CCC.txt"#), Some(("AAA.txt", "CCC.txt")));
+}
+
+fn add_quotes_if_with_whitespace(s: &str) -> String {
+    if s.contains(char::is_whitespace) {
+        format!("\"{}\"", s)
+    } else {
+        s.to_string()
+    }
 }
 
 fn process_line<W: Write>(out: &mut BufWriter<W>, split: bool, line: &str) -> bool {
@@ -49,7 +57,7 @@ fn process_line<W: Write>(out: &mut BufWriter<W>, split: bool, line: &str) -> bo
     }
 
     /*
-     * Parse the line (generic and rename case)
+     * Parse the line input
      *   XY pathname
      *   XY pathname1 -> pathname2
      */
@@ -67,14 +75,19 @@ fn process_line<W: Write>(out: &mut BufWriter<W>, split: bool, line: &str) -> bo
     let ch2 = status_code.chars().nth(1).unwrap();
     let deco_status_code = get_deco_status_code(ch1, ch2);
 
+    // Parse the pathname part (generic or rename case)
     if let Some((path1, path2)) = parse_rename(path_line) {
+        // rename case
+        let path1q = add_quotes_if_with_whitespace(path1);
+        let path2q = add_quotes_if_with_whitespace(path2);
         if split {
-            writeln!(out, "{} \x1b[0m{} :: \x1b[33m{}\x1b[0m", deco_status_code, path_line, path1).unwrap();
-            writeln!(out, "{} \x1b[0m{} :: \x1b[33m{}\x1b[0m", deco_status_code, path_line, path2).unwrap();
+            writeln!(out, "{} \x1b[0m{} :: \x1b[33m{}\x1b[0m", deco_status_code, path_line, path1q).unwrap();
+            writeln!(out, "{} \x1b[0m{} :: \x1b[33m{}\x1b[0m", deco_status_code, path_line, path2q).unwrap();
         } else {
-            writeln!(out, "{} \x1b[0m{} -> \x1b[33m{}\x1b[0m", deco_status_code, path1, path2).unwrap();
+            writeln!(out, "{} \x1b[0m{} -> \x1b[33m{}\x1b[0m", deco_status_code, path1q, path2q).unwrap();
         }
     } else {
+        // generic case
         writeln!(out, "{} \x1b[33m{}\x1b[0m", deco_status_code, path_line).unwrap();
     }
 
