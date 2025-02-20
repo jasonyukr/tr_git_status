a55a4dcf5bbdaead393743d54e45c05edea3b68e
Jason Yu <jasonyu.kr@gmail.com>
2025-02-20 11:57:25 +0900

    Split the big main() into two

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,63 @@ fn get_deco_status_code(ch1: char, ch2: char) -> String {
     }
 }
 
+fn process_line<W: Write>(out: &mut BufWriter<W>, split: bool, line: &str) {
+    if line.len() < 4 {
+        return;
+    }
+
+    let status_code = &line[0..2];
+    let sep = &line[2..3];
+    let path_line = &line[3..];
+
+    if sep != " " {
+        return;
+    }
+
+    let ch1;
+    if let Some(ch) = status_code.chars().nth(0) {
+        if ch == ' ' {
+            ch1 = '_';
+        } else {
+            ch1 = ch;
+        }
+    } else {
+        return;
+    }
+
+    let ch2;
+    if let Some(ch) = status_code.chars().nth(1) {
+        if ch == ' ' {
+            ch2 = '_';
+        } else {
+            ch2 = ch;
+        }
+    } else {
+        return;
+    }
+
+    let mut entry = ["", "", ""];
+    for (idx, e) in str::split_whitespace(path_line).enumerate() {
+        entry[idx] = e;
+        if idx >= 3 {
+            break;
+        }
+    }
+
+    let deco_status_code = get_deco_status_code(ch1, ch2);
+
+    if entry[1] == "->" {
+        if split {
+            writeln!(out, "{} \x1b[0m{} :: \x1b[33m{}\x1b[0m", deco_status_code, path_line, entry[0]).unwrap();
+            writeln!(out, "{} \x1b[0m{} :: \x1b[33m{}\x1b[0m", deco_status_code, path_line, entry[2]).unwrap();
+        } else {
+            writeln!(out, "{} \x1b[0m{} -> \x1b[33m{}\x1b[0m", deco_status_code, entry[0], entry[2]).unwrap();
+        }
+    } else {
+        writeln!(out, "{} \x1b[33m{}\x1b[0m", deco_status_code, path_line).unwrap();
+    }
+}
+
 fn main() {
     // parse argument
     let mut split = false;
@@ -22,66 +79,9 @@ fn main() {
     let mut out = BufWriter::new(stdout);
 
     let stdin = io::stdin();
-    for ln in stdin.lock().lines() {
-        let line;
-        match ln {
-            Ok(data) => line = data,
-            Err(_) => continue
-        }
-        
-        if line.len() < 4 {
-            continue;
-        }
-
-        let status_code = &line[0..2];
-        let sep = &line[2..3];
-        let path_line = &line[3..];
-
-        if sep != " " {
-            continue;
-        }
-
-        let ch1;
-        if let Some(ch) = status_code.chars().nth(0) {
-            if ch == ' ' {
-                ch1 = '_';
-            } else {
-                ch1 = ch;
-            }
-        } else {
-            continue;
-        }
-
-        let ch2;
-        if let Some(ch) = status_code.chars().nth(1) {
-            if ch == ' ' {
-                ch2 = '_';
-            } else {
-                ch2 = ch;
-            }
-        } else {
-            continue;
-        }
-
-        let mut entry = ["", "", ""];
-        for (idx, e) in str::split_whitespace(path_line).enumerate() {
-            entry[idx] = e;
-            if idx >= 3 {
-                break;
-            }
-        }
-
-        let deco_status_code = get_deco_status_code(ch1, ch2);
-
-        if entry[1] == "->" {
-            if split {
-                writeln!(out, "{} \x1b[0m{} :: \x1b[33m{}\x1b[0m", deco_status_code, path_line, entry[0]).unwrap();
-                writeln!(out, "{} \x1b[0m{} :: \x1b[33m{}\x1b[0m", deco_status_code, path_line, entry[2]).unwrap();
-            } else {
-                writeln!(out, "{} \x1b[0m{} -> \x1b[33m{}\x1b[0m", deco_status_code, entry[0], entry[2]).unwrap();
-            }
-        } else {
-            writeln!(out, "{} \x1b[33m{}\x1b[0m", deco_status_code, path_line).unwrap();
+    for line in stdin.lock().lines() {
+        if let Ok(ln) = line {
+            process_line(&mut out, split, &ln);
         }
     }
     out.flush().unwrap();
